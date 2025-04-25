4a7db418155490a6a595b8b3730350872b2d293e
adelcastillo-enginet <alejandro.delcastillo@enginetsystems.com>
2025-04-25 10:53:52 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+function App() {
+  const [selectedCountry, setSelectedCountry] = useState('');
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Registro de Usuario</h1>
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div>
+          <label htmlFor="name">Nombre:</label><br />
+          <input type="text" id="name" name="name" required />
+        </div>
+
+        <br />
+
+        <div>
+          <label htmlFor="country">País:</label><br />
+          <select
+            id="country"
+            name="country"
+            value={selectedCountry}
+            onChange={(e) => setSelectedCountry(e.target.value)}
+          >
+            <option value="">Seleccione un país</option>
+            {/* Aquí se deben agregar los options */}
+          </select>
+        </div>
+
+        <br />
+
+        <button type="submit">Registrar</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
